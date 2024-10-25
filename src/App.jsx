25feea7380051461fff4890045d96fbfa787fdd7
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-25 16:59:28 +0530

    create game play page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Welcome from './pages/WelcomePage/Welcome';
 import SignUp from './pages/SignUpPage/SignUp';
 import Login from './pages/LoginPage/Login';
+import GamePlay from './pages/GamePlayPage/GamePlay';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Welcome/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/game-play' element={<GamePlay/>}/>
       </Routes>
     </BrowserRouter>
   );
