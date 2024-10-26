a6d147cbf9a5a49b5c234e3b8f74c383cdc6b890
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-26 19:45:23 +0530

    Login page bug fix

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
       if(response.data){
         storeInSession('user', JSON.stringify(response.data));
-        toast.success('Logged in successfully');
+        // toast.success('Logged in successfully');
         navigate('/game-play')
       }
 
