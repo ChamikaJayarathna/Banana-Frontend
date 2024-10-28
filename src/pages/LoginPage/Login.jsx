2508bd276a1df09ba64731abd96098b1b5b2035c
Chamika Jayarathna <failgames.lk@gmail.com>
2024-10-28 10:11:05 +0530

    update loginpage UI

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,69 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast';
-import { storeInSession } from '../../components/Session';
+import Monkey from '../../assets/momkey02.png';
 
 const Login = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const navigate = useNavigate();
-
-  const userAuthServer = async () => {
-    try {
-      const response = await axios.post(import.meta.env.VITE_SERVER_DOMAIN + '/api/login', {
-        email,
-        password,
-      });
-
-      if(response.data){
-        storeInSession('user', JSON.stringify(response.data));
-        // toast.success('Logged in successfully');
-        navigate('/game-play')
-      }
-
-    } catch (error) {
-      toast.error(error.response?.data?.error || "An error occurred");
-    }
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-
-    if(!email.length){
-      return toast.error("Enter Email")
-    }
-
-    if(!emailRegex.test(email)){
-      return toast.error("Invalid Email")
-    }
-
-    if(!passwordRegex.test(password)){
-      return toast.error("Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters")
-    }
-
-    userAuthServer();
-
-  }
-
   return (
-    <div className="login-container">
-      <Toaster/>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1>Welcome Back</h1>
-        <div className="input-group">
-          <input  type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        <button type="submit" className="login-button">Login</button>
-      </form>
+    <>
+    <div className="login-wrapper">
+      <img src={Monkey} alt="image" className='mascot'/>
+      <div className="login-container">
+        <h1>Welcome!</h1>
+        <form action="">
+          <div className="input-group">
+            <label className='input-label' style={{ backgroundColor: '#C1DDE5' }}>E-mail Address</label>
+            <input type="email" placeholder='Enter your email' className='styled-input'/>
+          </div>
+          <div className="input-group">
+            <label className='input-label' style={{ backgroundColor: '#BBD9E0' }}>Password</label>
+            <input type="password" placeholder='Enter your password'className='styled-input'/>
+          </div>
+          <button type='submit' className='login-btn'>Login</button>
+        </form>
+      </div>
     </div>
+    </>
+    
   );
 }
 
