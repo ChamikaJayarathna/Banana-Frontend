fa07ffe9525cc5aba2ebdf1aa3f1a68c151bdbe3
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-02 19:30:24 +0530

    update times up componet

--- src/components/TimeUpCard/TimeUp.jsx
+++ src/components/TimeUpCard/TimeUp.jsx
@@ -18,6 +18,7 @@ const TimeUp = () => {
         </div>
         <h2 className="time-up-card-title">Time's Up!</h2>
         <p className="time-up-card-message">Play this if you wish to continue the Banana game.</p>
+        <Link to="" className="time-up-leaderboard-button">Leader Board</Link>
         <Link to="" className="time-up-play-button">Play</Link>
       </div>
     </div>
