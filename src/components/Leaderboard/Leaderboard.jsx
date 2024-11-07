ade14e11cf9aadc5e2e29c4b52d38b5107be74bf
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-07 23:56:10 +0530

    add navbar to leaderboard

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -3,6 +3,7 @@ import Monkey from '../../assets/momkey11.png';
 import LeaderboardCom from '../LeaderboardCom/LeaderboardCom';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 import './Leaderboard.css';
 
 const Leaderboard = () => {
@@ -26,28 +27,31 @@ const Leaderboard = () => {
   }, [mode]);
 
   return (
-    <div className="leaderboard-container">
-      <div className="leaderboard-header">
-        <div className="leaderboard-icon-container leaderboard-icon-container-left">
-          <img src={Monkey} alt="icon" className='leaderboard-monkey-icon' />
+    <>
+      <NavBar/>
+      <div className="leaderboard-container">
+        <div className="leaderboard-header">
+          <div className="leaderboard-icon-container leaderboard-icon-container-left">
+            <img src={Monkey} alt="icon" className='leaderboard-monkey-icon' />
+          </div>
+          <h1 className='leaderboard-header-title'>ACHIEVEMENTS</h1>
+          <div className="leaderboard-icon-container leaderboard-icon-container-right">
+            <img src={Monkey} alt="icon" className='leaderboard-monkey-icon' />
+          </div>
         </div>
-        <h1 className='leaderboard-header-title'>ACHIEVEMENTS</h1>
-        <div className="leaderboard-icon-container leaderboard-icon-container-right">
-          <img src={Monkey} alt="icon" className='leaderboard-monkey-icon' />
-        </div>
-      </div>
 
-      <div className="leaderboard-achievement-list">
-        {users.map((user, index) => (
-          <LeaderboardCom
-            key={index}
-            username={user.username}
-            score={user.score}
-            rank={index + 1}
-          />
-        ))}
+        <div className="leaderboard-achievement-list">
+          {users.map((user, index) => (
+            <LeaderboardCom
+              key={index}
+              username={user.username}
+              score={user.score}
+              rank={index + 1}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
