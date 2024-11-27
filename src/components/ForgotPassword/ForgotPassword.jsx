b70afaf1a690da289152b55a60c0cf04c6614220
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-27 10:59:21 +0530

    update forget password UI

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -3,8 +3,17 @@ import './ForgotPassword.css';
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <div className="forgot-password-container">
+      <form className="forgot-password-form">
+        <h1>Forgot Password</h1>
+        <div className="input-group">
+          <label className='input-label' style={{ backgroundColor: '#C1DDE5' }}>E-mail Address</label>
+          <input type="email" placeholder='Enter your email' className='styled-input'/>
+        </div>
+        <button className="forgot-password-reset-password">Reset Password</button>
+      </form>
+    </div>
   );
-}
+};
 
 export default ForgotPassword;
