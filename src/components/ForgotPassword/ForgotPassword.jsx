974507f1924a90969e8a24ca43e9820e05ed31f3
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-27 13:54:18 +0530

    create forgot password logic

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
 import './ForgotPassword.css';
+import axios from 'axios';
 
 const ForgotPassword = () => {
-  return (
-    <div className="forgot-password-container">
-      <form className="forgot-password-form">
-        <h1>Forgot Password</h1>
-        <div className="input-group">
-          <label className='input-label' style={{ backgroundColor: '#C1DDE5' }}>E-mail Address</label>
-          <input type="email" placeholder='Enter your email' className='styled-input'/>
-        </div>
-        <button className="forgot-password-reset-password">Reset Password</button>
-      </form>
-    </div>
-  );
+    const forgotPasswordForm = useRef();
+    const location = useLocation();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let form = new FormData(forgotPasswordForm.current);
+        let { email } = Object.fromEntries(form.entries());
+
+        if (!email) {
+            return toast.error('Please enter your email address');
+        }
+
+        axios
+            .post(import.meta.env.VITE_SERVER_DOMAIN + "/api/forgot-password", { email })
+            .then(() => {
+                toast.success('Password reset link sent to your email');
+            })
+            .catch(({ response }) => {
+                toast.error(response.data.error || "An error occurred. Please try again.");
+            });
+    };
+
+    return (
+        <>
+            <Toaster />
+            <div className="forgot-password-container">
+                <form ref={forgotPasswordForm} className="forgot-password-form">
+                    <h1>Forgot Password</h1>
+                    <div className="input-group">
+                        <label className='input-label' style={{ backgroundColor: '#C1DDE5' }}>E-mail Address</label>
+                        <input type="email" name="email" placeholder='Enter your email' className='styled-input' />
+                    </div>
+                    <button className="forgot-password-reset-password" type='submit' onClick={handleSubmit}>
+                        Reset Password
+                    </button>
+                </form>
+            </div>
+        </>
+    );
 };
 
 export default ForgotPassword;
