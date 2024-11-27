c3e6ad6b7336fe432a8b2e362f5800fb6d64b046
Chamika Jayarathna <failgames.lk@gmail.com>
2024-11-27 15:42:15 +0530

    update reset password UI

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ResetPassword.css';
 
 const ResetPassword = () => {
+
+    const [newShowPassword, setNewShowPassword] = useState(false);
+    const [confirmShowPassword, setConfirmShowPassword] = useState(false);
+
   return (
-    <div>ResetPassword</div>
+    <div className='reset-password-container'>
+        <form action="">
+
+            <h1>Reset Password</h1>
+
+            <div className="input-group">
+              <label className='input-label' style={{ backgroundColor: '#BBD9E0'}}>New Password</label>
+              <div className="password-wrapper">
+                <input type={newShowPassword ? "text" : "password"} placeholder='New Password'className='styled-input'/>
+                <i className={`fi ${newShowPassword ? 'fi-rr-eye' : 'fi-rr-eye-crossed'} password-toggle-icon`} onClick={() => setNewShowPassword(!newShowPassword)}></i>
+              </div>
+            </div>
+
+            <div className="input-group">
+              <label className='input-label' style={{ backgroundColor: '#BBD9E0'}}>Confirm Password</label>
+              <div className="password-wrapper">
+                <input type={confirmShowPassword ? "text" : "password"} placeholder='Confirm Password'className='styled-input'/>
+                <i className={`fi ${confirmShowPassword ? 'fi-rr-eye' : 'fi-rr-eye-crossed'} password-toggle-icon`} onClick={() => setConfirmShowPassword(!confirmShowPassword)}></i>
+              </div>
+            </div>
+
+            <button type='submit' className='reset-password-btn'>
+                Reset Password
+            </button>
+
+        </form>
+    </div>
   );
 }
 
